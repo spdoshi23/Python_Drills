33c63b2529d4f56d4f0b4b1ea3093cc94e6d8df1
Shubham Doshi <spdoshi23@gmail.com>
2026-09-11 18:53:54 +0530

    drill4

--- drill_24.py
+++ drill_24.py
@@ -1,10 +1,26 @@
 N = int(input("Enter a number N: "))
 
-while N>0:
+previous = N % 10
+N = N // 10
+
+increasing = True 
+
+
+while N>0: 
     digit = N%10
+    if digit>=previous:
+        increasing = False 
+        break
     previous = digit
     N = N//10
-    if digit>=previous:
-        print("number is not strictly increasing")
-    else:
-        print("number is strictly increasing")
+
+if increasing==False:
+    print("Number is not increasing")
+else:
+    print("Number is increasing")
+
+
+    
+    
+        
+
